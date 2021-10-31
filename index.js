f00b97cc69859cedbc09fbd0dc0c09617470627d
Joe Sembler <joeysembler@gmail.com>
2021-10-31 15:31:29 -0400

    Completed Assignment

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
-// Code your solutions in this file
+// Joe Sembler
+// Flatiron School
+// Phase 0: "Intro to JavaScript 2 - Looping Code Along"
+
+function writeCards(names){
+    const thankYou = [];
+    let i = 0;
+    while (i < 3){
+        thankYou[i] = "Thank you, " + names[i] + ", for the wonderful surprise gift!"; 
+        i++; 
+    }
+
+    return thankYou;
+}
+
+function countDown(num){
+    //let i = 0;
+    while(num > -1){
+        console.log(num);
+        num--;  
+    }
+    return null;
+}
